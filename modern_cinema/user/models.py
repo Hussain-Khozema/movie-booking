fb8e09285d0f44ac8635f158274940f4b1d96e6f
Hussain <hussainkk55@gmail.com>
2021-08-27 23:30:51 +0800

    added logout

--- modern_cinema/user/models.py
+++ modern_cinema/user/models.py
@@ -9,8 +9,8 @@ from django.core.validators import RegexValidator
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     city = models.CharField(max_length=20)
-    phone_regex = RegexValidator(regex=r'^[7-9]\d{9}$', message="Phone number must be of 10 digits")
-    phone_number = models.CharField(validators=[phone_regex], blank=True, max_length=10, null=True)
+    phone_regex = RegexValidator(regex=r'^[8-9]\d{7}$', message="Phone number must be of 8 digits")
+    phone_number = models.CharField(validators=[phone_regex], blank=True, max_length=8, null=True)
 
     def __str__(self):
         return self.user.username
