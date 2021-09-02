b3c32a46b10d985175b5a72916072ecde414878e
Jeremy book <jeremy.b@live.com.sg>
2021-09-02 22:16:40 +0800

    added password confirmation field

--- modern_cinema/user/forms.py
+++ modern_cinema/user/forms.py
@@ -5,6 +5,17 @@ from .models import UserProfile
 
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
+    confirm_password = forms.CharField(widget=forms.PasswordInput())
+
+    def clean(self):
+        cleaned_data = super(UserForm, self).clean()
+        password = cleaned_data.get("password")
+        confirm_password = cleaned_data.get("confirm_password")
+
+        if password != confirm_password:
+            raise forms.ValidationError(
+                "passwords does not match"
+            )
 
     class Meta:
         model = User
