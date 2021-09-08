2da3f8cd6c8219ade276eddc87680950e078afbd
Jeremy book <jeremy.b@live.com.sg>
2021-09-08 15:05:55 +0800

    integrated recommender model

--- modern_cinema/home/apps.py
+++ modern_cinema/home/apps.py
@@ -1,6 +1,117 @@
 from django.apps import AppConfig
-
+import torch
+from torch.utils.data import Dataset
+import torch.nn as nn
+import pytorch_lightning as pl
+from torch.utils.data import DataLoader
+import numpy as np
 
 class HomeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'home'
+
+class MovieLensTrainDataset(Dataset):
+    """MovieLens PyTorch Dataset for Training
+
+    Args:
+        ratings (pd.DataFrame): Dataframe containing the movie ratings
+        all_movieIds (list): List containing all movieIds
+
+    """
+
+    def __init__(self, ratings, all_movieIds):
+        self.users, self.items, self.labels = self.get_dataset(ratings, all_movieIds)
+
+    def __len__(self):
+        return len(self.users)
+
+    def __getitem__(self, idx):
+        return self.users[idx], self.items[idx], self.labels[idx]
+
+    def get_dataset(self, ratings, all_movieIds):
+        users, items, labels = [], [], []
+        user_item_set = set(zip(ratings['userId'], ratings['movieId']))
+
+        num_negatives = 4
+        for u, i in user_item_set:
+            users.append(u)
+            items.append(i)
+            labels.append(1)
+            for _ in range(num_negatives):
+                negative_item = np.random.choice(all_movieIds)
+                while (u, negative_item) in user_item_set:
+                    negative_item = np.random.choice(all_movieIds)
+                users.append(u)
+                items.append(negative_item)
+                labels.append(0)
+
+        return torch.tensor(users), torch.tensor(items), torch.tensor(labels)
+
+class NCF(pl.LightningModule):
+    """ Neural Collaborative Filtering (NCF)
+
+        Args:
+            num_users (int): Number of unique users
+            num_items (int): Number of unique items
+            ratings (pd.DataFrame): Dataframe containing the movie ratings for training
+            all_movieIds (list): List containing all movieIds (train + test)
+    """
+
+    def __init__(self, num_users, num_items, ratings, all_movieIds):
+        super().__init__()
+        self.user_embedding = nn.Embedding(num_embeddings=num_users, embedding_dim=8)
+        self.item_embedding = nn.Embedding(num_embeddings=num_items, embedding_dim=8)
+        self.fc1 = nn.Linear(in_features=16, out_features=64)
+        self.fc2 = nn.Linear(in_features=64, out_features=32)
+        self.output = nn.Linear(in_features=32, out_features=1)
+        self.ratings = ratings
+        self.all_movieIds = all_movieIds
+
+    def forward(self, user_input, item_input):
+
+        # Pass through embedding layers
+        user_embedded = self.user_embedding(user_input)
+        item_embedded = self.item_embedding(item_input)
+
+        # Concat the two embedding layers
+        vector = torch.cat([user_embedded, item_embedded], dim=-1)
+
+        # Pass through dense layer
+        vector = nn.ReLU()(self.fc1(vector))
+        vector = nn.ReLU()(self.fc2(vector))
+
+        # Output layer
+        pred = nn.Sigmoid()(self.output(vector))
+
+        return pred
+
+    def training_step(self, batch, batch_idx):
+        user_input, item_input, labels = batch
+        predicted_labels = self(user_input, item_input)
+        loss = nn.BCELoss()(predicted_labels, labels.view(-1, 1).float())
+        return loss
+
+    def configure_optimizers(self):
+        return torch.optim.Adam(self.parameters())
+
+    def train_dataloader(self):
+        return DataLoader(MovieLensTrainDataset(self.ratings, self.all_movieIds),
+                          batch_size=512, num_workers=4)
+
+class Recommender():
+  def __init__(self):
+    self.model = torch.load('home/recommender_model')
+
+  def predict(self, userId, movie_list):
+    # get length of movie list
+    movie_list_len = len(movie_list)
+
+    # use model to predict user's preference sorted from most like to least like
+    sorting_array = np.argsort(np.squeeze(self.model(torch.tensor([userId]*movie_list_len), torch.tensor(movie_list)).detach().numpy()))[::-1].tolist()
+
+    # sort movies with sortin array
+    zipped_lists = zip(sorting_array, movie_list)
+    sorted_zipped_lists = sorted(zipped_lists)
+    sorted_list = [element for _, element in sorted_zipped_lists]
+
+    return(sorted_list)
--- modern_cinema/home/views.py
+++ modern_cinema/home/views.py
@@ -2,7 +2,18 @@ from django.http import Http404
 from django.shortcuts import render
 from movie.models import Movie
 from theatre.models import Show
+from user.models import User
+from user.models import UserProfile
+from home.apps import Recommender
 import datetime
+
+import torch
+from torch.utils.data import Dataset
+import torch.nn as nn
+import pytorch_lightning as pl
+from torch.utils.data import DataLoader
+import numpy as np
+
 
 
 # Create your views here.
@@ -11,7 +22,49 @@ def movie_list(request):
     movie_list = []
     for i in range(0, len(movies)):
         movie_list.append(movies[i])
-    return render(request, 'movie/movie_list.html', {'movies': movie_list})
+
+    #recommender here
+    model = Recommender()
+
+    all_movie_ids_list = Movie.objects.values_list('movie_id',flat=True) #get movie_id attributes of each Movie object
+    all_movie_ids_list = list(map(int, all_movie_ids_list)) # convert string elements to int
+
+    recommended_movie_list = []
+
+    logged_in = False
+
+    try:
+        current_user_id = int(UserProfile.objects.filter(user=request.user).values_list('model_input_user_id',flat=True)[0]) # get logged in user_id
+        logged_in = True
+        # print(Movie.objects.filter(movie_id=2)) # filter Movie objects by movie_id
+        # print(request.user) # print username of current logged in user
+        # print(User.objects.filter(username=request.user)) # get logged in user's User objects
+        # print(User.objects.values_list('username',flat=True)) # get all username in database
+        # print(UserProfile.objects.values_list('model_input_user_id',flat=True)) # get all user's model_input_user_id
+        # print(model.predict(userId=33152,movie_list=all_movie_ids_list)[:4])
+        # print(model.predict(userId=25534,movie_list=all_movie_ids_list)[:4])
+        # print(model.predict(userId=1423,movie_list=all_movie_ids_list)[:4])
+        # print(model.predict(userId=3421,movie_list=all_movie_ids_list)[:4])
+
+        recommender_output_list = model.predict(userId=current_user_id,movie_list=all_movie_ids_list)[:4]
+
+        for movie_id_ in recommender_output_list:
+            recommended_movie_list.append(Movie.objects.get(movie_id=movie_id_))
+
+    # if user not logged in or no not user_id assigned
+    except:
+        pass
+
+    #recommender ends
+
+
+    context = {
+        'movies': movie_list,
+        'recommended_movies': recommended_movie_list,
+        'logged_in': logged_in
+    }
+
+    return render(request, 'movie/movie_list.html', context)
 
 # def movie_list(request):
 #     movies = Movie.objects.all().order_by('language')
