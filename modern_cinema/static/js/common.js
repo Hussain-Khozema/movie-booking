82b2065590d6a947d021df00bf752e1b74f0ded1
Hussain <hussainkk55@gmail.com>
2021-09-30 11:45:19 +0800

    changes

--- modern_cinema/static/js/common.js
+++ modern_cinema/static/js/common.js
@@ -18,31 +18,49 @@ function watchTrailer(name,url){
 	$('<div />').html(html);
 }
 
-
 function createSeatArray() {
 	var seats_per_row = 15;
-	var seats = {'Platinum':2, 'Gold':2, 'Silver':6};
-	var rows = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O']
-	var seat_array = "<div id='seat_array'>";
+	var rows = ['A','B','C','D','E','F','G','H']
+	var seat_array = "<div id='seat_array'><table>";
 	var count = 0;
-	for(var key in seats)
+	for(var row in rows)
 	{
-		seat_array += "<table class='"+key+"'>";
-		for(var i=0; i<seats[key]; i++)
+		seat_array += "<tr>";
+		for(var j=1; j<=seats_per_row; j++)
 		{
-			seat_array += "<tr class='"+rows[count]+"'>";
-			for(var j=1; j<=seats_per_row; j++)
-			{
-				seat_array += "<td><div class='"+key+"'><a>"+rows[count]+j+"</a></div></td>";
-			}
-			seat_array += "</tr>";
-			count++;
+				seat_array += "<td><div class='selectable'><a>"+rows[count]+j+"</a></div></td>";
 		}
-			seat_array += "</table>";
+		seat_array += "</tr>";
+		count++;
 		}
-		seat_array += "</div>"
+	seat_array += "</table></div>";
 	return seat_array;
 }
+
+// function createSeatArray() {
+// 	var seats_per_row = 15;
+// 	var seats = {'Platinum':2, 'Gold':2, 'Silver':6};
+// 	var rows = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O']
+// 	var seat_array = "<div id='seat_array'>";
+// 	var count = 0;
+// 	for(var key in seats)
+// 	{
+// 		seat_array += "<table class='"+key+"'>";
+// 		for(var i=0; i<seats[key]; i++)
+// 		{
+// 			seat_array += "<tr class='"+rows[count]+"'>";
+// 			for(var j=1; j<=seats_per_row; j++)
+// 			{
+// 				seat_array += "<td><div class='"+key+"'><a>"+rows[count]+j+"</a></div></td>";
+// 			}
+// 			seat_array += "</tr>";
+// 			count++;
+// 		}
+// 			seat_array += "</table>";
+// 		}
+// 		seat_array += "</div>"
+// 	return seat_array;
+// }
 
 $(document).ready(function(){
 	//create seat array
